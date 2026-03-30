5d302ad8aeba6626629fc285ec4ffc90efec827a
JacobPham1 <pham.jacob3@gmail.com>
2026-03-30 13:34:18 -0500

    install and configure Tailwind and shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">Tailwind is working!</h1>
+    <div className="flex items-center justify-center h-screen">
+      <Button>shadcn is working!</Button>
     </div>
   )
 }
