f4b362b392e6c4d782107a18566ded17b1903d26
JacobPham1 <pham.jacob3@gmail.com>
2026-03-30 13:46:59 -0500

    clean up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button"
-
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Button>shadcn is working!</Button>
+    <div>
+      <h1>OpTraxx</h1>
     </div>
   )
 }
